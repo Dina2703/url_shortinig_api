5ba4ad7983dde16d044d6252fbbc59f1c8acb33a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-22 21:51:05 -0500

    StatsSection font size changes

--- src/components/StatsSection.js
+++ src/components/StatsSection.js
@@ -7,11 +7,11 @@ function StatsSection() {
   return (
     <div className="bg-slate-100 px-6 pb-16 relative" id="stats">
       <div className="max-w-6xl lg:mx-auto  px-3 md:px-5 lg:px-12 flex flex-col justify-center text-center my-3 relative z-10">
-        <div className="my-7">
-          <h1 className="font-bold pt-5 md:text-xl lg:text-2xl">
-            Advanced Statictics
+        <div className="mt-7 mb-14">
+          <h1 className="font-bold pt-5 md:text-xl text-[22px] lg:text-2xl">
+            Advanced Statistics
           </h1>
-          <p className="text-darkGrayishBlue text-sm  md:text-lg py-6 px-2 max-w-lg lg:mx-auto ">
+          <p className="text-darkGrayishBlue text-[14px]  md:text-lg py-3  max-w-lg lg:mx-auto ">
             Track how your links are performing across the web with our advanced
             statistics dashboard.
           </p>
@@ -22,10 +22,10 @@ function StatsSection() {
             <div className="h-16 w-16 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  right-0 left-0">
               <img src={iconBrandRecognition} alt="" className="object-cover" />
             </div>
-            <div className="font-bold pt-5 text-sm lg:text-xl">
+            <div className="font-bold pt-5 text-[18px] lg:text-xl">
               Brand Recognition
             </div>
-            <div className="text-darkGrayishBlue text-sm  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
+            <div className="text-darkGrayishBlue text-[14px]  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
               Boost your brand recogniton with each click. Generic links don't
               mean a thing. Branded links help install confidence in your
               content.
@@ -35,10 +35,10 @@ function StatsSection() {
             <div className="h-16 w-16 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  right-0 left-0">
               <img src={iconDetailedRecords} alt="" className="object-cover" />
             </div>
-            <div className="font-bold pt-5 text-sm lg:text-xl">
+            <div className="font-bold pt-5 text-[18px]  lg:text-xl">
               Detailed Records
             </div>
-            <div className="text-darkGrayishBlue text-sm  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
+            <div className="text-darkGrayishBlue text-[14px]  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
               Gain insights into who is clicking your links. Knowing when and
               where people engaging with your content hepls inform better
               decisions.
@@ -52,10 +52,10 @@ function StatsSection() {
                 className="object-cover"
               />
             </div>
-            <div className="font-bold pt-5 text-sm lg:text-xl">
+            <div className="font-bold pt-5 text-[18px] lg:text-xl">
               Detailed Records
             </div>
-            <div className="text-darkGrayishBlue text-sm  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
+            <div className="text-darkGrayishBlue text-[14px]  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
               Improve brand awareness and content discoverability through
               customizable links, supercharging audience engagement.
             </div>
