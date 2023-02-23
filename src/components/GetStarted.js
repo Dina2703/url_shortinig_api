28251dfbf071ad0cc1cf3dc0ef191ee173b8c3ea
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-22 22:42:00 -0500

    Boost/ GetStarted Component design

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ButtonComponent from "./ButtonComponent";
 
 function GetStarted() {
-  return <div>GetStarted</div>;
+  return (
+    <div id="boost" className="w-full h-48 bg-darkViolet  relative">
+      <div className="relative z-50 flex flex-col items-center h-full justify-center">
+        <h1 className="text-[24px] font-bold text-white mb-3">
+          Boost your links today
+        </h1>
+        <div>
+          <ButtonComponent text="Get Started" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GetStarted;
