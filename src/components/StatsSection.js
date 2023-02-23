809b386769bf766b433cba1014f38f8f85657218
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-22 22:19:03 -0500

    StatsSection design for the large screens

--- src/components/StatsSection.js
+++ src/components/StatsSection.js
@@ -17,45 +17,45 @@ function StatsSection() {
           </p>
         </div>
 
-        <div className="flex flex-col gap-16">
-          <div className="bg-white  p-8 rounded relative ">
-            <div className="h-16 w-16 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  right-0 left-0">
+        <div className="flex flex-col md:flex-row gap-16 md:relative md:gap-8 md:mt-6">
+          <div className="bg-white  p-8 md:p-[2.5%] rounded relative md:bottom-10">
+            <div className="h-14 w-14 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  md:right-[60%] right-0 left-0 ">
               <img src={iconBrandRecognition} alt="" className="object-cover" />
             </div>
-            <div className="font-bold pt-5 text-[18px] lg:text-xl">
+            <div className="font-bold pt-5 text-[18px] md:text-[20px] md:text-start ">
               Brand Recognition
             </div>
-            <div className="text-darkGrayishBlue text-[14px]  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
+            <div className="text-darkGrayishBlue text-[14px] md:text-[16px] pt-4 max-w-lg lg:mx-auto md:text-start">
               Boost your brand recogniton with each click. Generic links don't
               mean a thing. Branded links help install confidence in your
               content.
             </div>
           </div>
-          <div className="bg-white  p-8 rounded relative ">
-            <div className="h-16 w-16 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  right-0 left-0">
+          <div className="bg-white  p-8 md:p-[2.5%] rounded relative md:top-0">
+            <div className="h-14 w-14 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  md:right-[60%] right-0  left-0">
               <img src={iconDetailedRecords} alt="" className="object-cover" />
             </div>
-            <div className="font-bold pt-5 text-[18px]  lg:text-xl">
+            <div className="font-bold pt-5 text-[18px] md:text-[20px] md:text-start ">
               Detailed Records
             </div>
-            <div className="text-darkGrayishBlue text-[14px]  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
+            <div className="text-darkGrayishBlue text-[14px] md:text-[16px] pt-4 max-w-lg lg:mx-auto md:text-start">
               Gain insights into who is clicking your links. Knowing when and
               where people engaging with your content hepls inform better
               decisions.
             </div>
           </div>
-          <div className="bg-white  p-8 rounded relative ">
-            <div className="h-16 w-16 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  right-0 left-0">
+          <div className="bg-white p-8 md:p-[2.5%] rounded relative md:top-10">
+            <div className="h-14 w-14 p-4 mx-auto rounded-full bg-darkViolet absolute -top-8  md:right-[60%] right-0 left-0">
               <img
                 src={iconFullyCustomizable}
                 alt=""
                 className="object-cover"
               />
             </div>
-            <div className="font-bold pt-5 text-[18px] lg:text-xl">
-              Detailed Records
+            <div className="font-bold pt-5 text-[18px] md:text-[20px] md:text-start">
+              Fully Customizable
             </div>
-            <div className="text-darkGrayishBlue text-[14px]  md:text-lg py-4 px-2 max-w-lg lg:mx-auto">
+            <div className="text-darkGrayishBlue text-[14px] md:text-[16px] pt-4 max-w-lg lg:mx-auto md:text-start">
               Improve brand awareness and content discoverability through
               customizable links, supercharging audience engagement.
             </div>
