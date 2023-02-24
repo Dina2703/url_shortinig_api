d17f2c39dd5e050e7bf84a7057f2bb7ea7dc5bfb
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-23 20:48:39 -0500

    design a Dropdown menu  for the mobile  version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import logo from "../images/logo.svg";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <section
       id="navbar_section "
       className="px-3 md:px-5 lg:px-12 max-w-6xl lg:mx-auto"
     >
-      <nav className=" h-20 mx-auto flex items-center  justify-between">
+      <nav className=" h-20 mx-auto flex items-center  justify-between ">
         <a href="#" className="mr-8">
           <img src={logo} alt="logo" />
         </a>
@@ -22,7 +28,7 @@ function Navbar() {
             Resources
           </li>
         </ul>
-        <div className="flex space-x-2">
+        <div className="md:flex  hidden space-x-2  pl-3">
           <button className="w-[5rem] bottom-5 bg-white p-1.5 rounded-full  font-[700] text-sm hover:text-veryDarkViolet ease-in-out duration-150 text-grayishViolet ">
             Login
           </button>
@@ -30,7 +36,33 @@ function Navbar() {
             Sign Up
           </button>
         </div>
+        <div
+          className="flex flex-col md:hidden w-6  gap-1.5 cursor-pointer relative"
+          onClick={toggleMenu}
+        >
+          <span className="border-b-[4px] rounded-lg border-grayishViolet" />
+          <span className="border-b-[4px] rounded-lg border-grayishViolet" />
+          <span className="border-b-[4px] rounded-lg border-grayishViolet" />
+        </div>
       </nav>
+      {/* module */}
+      {isMenuOpen && (
+        <div className="w-3/4 bg-darkViolet mx-auto text-white rounded-lg p-6 absolute z-50 right-0 left-0">
+          <ul className="w-full flex flex-col justify-center items-center gap-2 font-bold text-sm ">
+            <li className="cursor-pointer hover:text-cyan">Features</li>
+            <li className="cursor-pointer hover:text-cyan">Pricing</li>
+            <li className="cursor-pointer hover:text-cyan">Resources</li>
+            <li className="cursor-pointer hover:text-cyan">Features</li>
+
+            <li className="border-t-[.5px] border-grayishViolet  w-full text-center pt-[16px] mt-[16px] cursor-pointer hover:text-cyan">
+              Login
+            </li>
+            <button className=" p-1.5 w-full   rounded-full  font-bold text-sm bg-cyan hover:bg-opacity-70 my-2">
+              Sign Up
+            </button>
+          </ul>
+        </div>
+      )}
     </section>
   );
 }
