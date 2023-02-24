6c57d51108e9cc6bea6fe81fcb23a67cd09e4183
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-24 16:40:00 -0500

    shortened link functionality: test to create an object for each link and display it. using uuid to generate random id.

--- src/components/ShortenItSection.js
+++ src/components/ShortenItSection.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ShortenedCard from "./ShortenedCard";
 import axios from "axios";
 import clsx from "clsx";
+import uuid from "react-uuid";
 
 function ShortenItSection() {
   const [inputValue, setInputValue] = useState("");
   const [link, setLink] = useState("");
+  const [linkArr, setlinkArr] = useState([]);
   const [shortLink, setShortLink] = useState("");
   const [errMsg, setErrMsg] = useState("");
 
   // console.log(link);
-  useEffect(() => {
+
+  const shortenLink = async () => {
     const options = {
       method: "POST",
       url: "https://url-shortner6.p.rapidapi.com/shorten",
@@ -19,7 +22,8 @@ function ShortenItSection() {
         "X-RapidAPI-Key": "195864ad65msh969ab9f081566ebp130455jsnbf226eb5209a",
         "X-RapidAPI-Host": "url-shortner6.p.rapidapi.com",
       },
-      data: `{"fullUrl":${JSON.stringify(link)}}`,
+      // data: `{"fullUrl":${JSON.stringify(link)}}`,
+      data: '{"fullUrl":"https://rapidapi.com"}',
     };
 
     axios
@@ -31,7 +35,7 @@ function ShortenItSection() {
       .catch(function (error) {
         console.error(error);
       });
-  }, [link]);
+  };
 
   const handleSubmit = () => {
     if (inputValue.length <= 0) {
@@ -39,22 +43,44 @@ function ShortenItSection() {
     } else {
       setLink(inputValue);
       console.log(inputValue);
+      shortenLink();
     }
   };
 
+  const addLink = () => {
+    setLink(inputValue);
+    setShortLink(inputValue);
+    setlinkArr((prev) => [
+      ...prev,
+      {
+        id: uuid(),
+        shortLink: inputValue,
+        fullLink: inputValue,
+      },
+    ]);
+    setInputValue("");
+  };
+
+  // get input value on enter
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addLink();
+    }
+  };
+
+  console.log(linkArr);
+
   return (
-    <div id="shorten_window" className="mt-24  md:mt-0 bg-slate-100 relative">
+    <div id="shorten_window" className="mt-24  2xl:mt-0 bg-slate-100 relative">
       <div className="relative -top-12 z-40 left-0 right-0 flex flex-col gap-4 ">
         <div
           id="input_container"
-          className="w-3/4 p-4 md:p-8 rounded bg-darkViolet flex flex-col md:flex-row justify-center mx-auto gap-4 relative max-w-3xl "
+          className="w-3/4 p-4 md:p-8 rounded bg-darkViolet flex flex-col md:flex-row justify-center mx-auto gap-6 md:gap-4 relative max-w-3xl "
         >
           <div className="md:flex-1 h-10">
             <input
               type="text"
               placeholder="Shorten a link here... "
-              //   className="rounded  h-full text-[16px]  px-3 placeholder:text-red
-              // outline-none  w-full"
               className={clsx(
                 "rounded",
                 "h-full",
@@ -67,26 +93,32 @@ function ShortenItSection() {
                 errMsg && "border-red"
               )}
               value={inputValue}
+              onMouseDown={() => setErrMsg("")}
               onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleKeyDown}
             />
-            <p className="text-red text-[10px] italic mt-1">
+            <p className="text-red text-[10px] italic mt-1 ">
               {errMsg ? errMsg : ""}
             </p>
           </div>
 
           <button
-            onClick={handleSubmit}
+            // onClick={handleSubmit}
+            onClick={addLink}
             type="submit"
             className="rounded py-1.5 px-3 text-[16px] bg-cyan text-white hover:bg-opacity-70 md:w-28"
           >
             Shorten It!
           </button>
         </div>
-
         {/* shortened links */}
-        {link && shortLink && (
-          <ShortenedCard link={link} shortLink={shortLink} />
-        )}
+        {linkArr.reverse().map((each) => (
+          <ShortenedCard
+            key={each.id}
+            fullLink={each.fullLink}
+            shortLink={each.shortLink}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/ShortenedCard.js
+++ src/components/ShortenedCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function ShortenedCard({ link, shortLink }) {
+function ShortenedCard({ fullLink, shortLink }) {
   return (
     <div>
       <div className="w-3/4 rounded   bg-white  mx-auto  max-w-3xl flex flex-col md:flex-row ">
         <div className="rounded-t-lg py-2 px-3 text-[15px]  border-b-2 border-slate-100 md:flex-1 md:flex md:justify-between md:items-center">
-          {link}
+          {fullLink}
         </div>
         <div className=" py-2 px-3 md:my-auto text-[15px] text-cyan   ">
           {shortLink}
